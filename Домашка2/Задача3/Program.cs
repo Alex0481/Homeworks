216a56c57f86111de8458b4b8ccb8fae50a33724
Alex0481 <moscow1981@mail.ru>
2022-02-03 18:33:25 +0300

    Домашка 2

---
+++ Домашка2/Задача3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Число четное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное");
+            }
+        }
+    }
+}
---
+++ Домашка2/задача1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задача1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите минимальную температуру за сутки");
+            int mintemp = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите максимальную температуру за сутки");
+            int maxtemp = int.Parse(Console.ReadLine());
+            int averagetemp = (mintemp + maxtemp) / 2;
+            Console.WriteLine($"Средняя температура за сутки {averagetemp} градусов");
+            Console.ReadKey();
+        }
+    }
+}
